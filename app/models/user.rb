5fce4a54c18264d2a1fc4be2bb74e5d7fb4a40b9
Eric Anderson <ande8135@gmail.com>
2016-12-29 05:23:19 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
     
-  validates :password,  length: { minimum: 6 },
+  validates :password,  length: { minimum: 5 },
                         presence: true
     
   
